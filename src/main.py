c5df084266701ffd239a47c5fbb285c8a079c27d
Vlad <vlad.voskanyan@gmail.com>
2026-01-07 23:03:47 -0500

    Add recursive implementation for any page

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from textnode import TextNode, TextType
-from website_prep import recursive_copy_to_destination, generate_page
+from website_prep import recursive_copy_to_destination, generate_pages_recursive
 
 def main():
     recursive_copy_to_destination("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 if __name__ == "__main__":
     main()
--- src/website_prep.py
+++ src/website_prep.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import shutil
 
 from htmlnode import HTMLNode
@@ -46,3 +47,20 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dir_of_dest_path)
     with open(dest_path, "w") as f:
         f.write(full_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dir_abs_path = os.path.abspath(dir_path_content)
+    dest_dir_path_content = os.path.abspath(dest_dir_path)
+    list_of_paths = os.listdir(dir_abs_path)
+    for path in list_of_paths:
+        abs_path = os.path.abspath(os.path.join(dir_path_content, path))
+        if os.path.isfile(abs_path):
+            parts = abs_path.split(".")
+            if parts[-1] == "md":
+                dest_dir_file_path = os.path.abspath(os.path.join(dest_dir_path_content, path))
+                dest_html_path = pathlib.Path(dest_dir_file_path).with_suffix(".html")
+                generate_page(abs_path, template_path, dest_html_path)
+        else:
+            new_dir_path_content = os.path.abspath(os.path.join(dir_path_content, path))
+            new_dest_dir_path = os.path.abspath(os.path.join(dest_dir_path, path))
+            generate_pages_recursive(new_dir_path_content, template_path, new_dest_dir_path)
